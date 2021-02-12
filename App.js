3a55f1d6953abb4663773bbcc7d85e96e57fa10d
Cesar F <beguface@gmail.com>
2021-02-12 15:13:28 -0400

    added recieve push notification

--- App.js
+++ App.js
@@ -14,14 +14,49 @@ import {
   Text,
   Button,
   StatusBar,
+  Alert,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 
+import messaging from '@react-native-firebase/messaging';
+
 const App: () => React$Node = () => {
   const [user, setUser] = useState('No one :(');
+
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  };
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.log('Your Firebase Token is:', fcmToken);
+    } else {
+      console.log('Failed', 'No token received');
+    }
+  };
+
+  // firebase push notification
+  useEffect(() => {
+    requestUserPermission();
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  });
 
   //  firebase dynamic linking listener
   useEffect(() => {
